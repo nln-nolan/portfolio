96cf2bf54db4b28eb93d5e77886b30edae04da60
nln-nolan <131177828+nln-nolan@users.noreply.github.com>
2023-11-06 23:34:58 +0100

    add Carrousel in "my-project"

---
+++ JS/Forms.js
@@ -0,0 +1,10 @@
+// Reset Forms
+function resetForm() {
+    document.getElementById("form").reset();
+}
+
+
+// Select2 : bar de recherche dans le forms contact
+$(document).ready(function() {
+    $("#mySelect").select2();
+});
