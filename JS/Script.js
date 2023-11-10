e3ce6151795460f8e19d85ebb0377e5f2f1e561b
nln-nolan <131177828+nln-nolan@users.noreply.github.com>
2023-11-10 11:10:50 +0100

    Delete JS directory

--- JS/Script.js
+++
@@ -1,5 +0,0 @@
-// Copyright JS
-console.log('╔════════════════════════════════════════════════╗')
-console.log('║           Portfolio created 10/2023            ║')
-console.log('║  © Copyright by nln.nolan All Rights Reserved. ║ ')
-console.log('╚════════════════════════════════════════════════╝')
